287993e46239f3b30dbbd8b77a945fe08ff4d0c6
jjalseu <cjfwns966@gmail.com>
2024-11-08 00:14:25 +0900

    - 할일 목록 아이템 컨테이너 감싸는 태크 Link 태그로 변경

--- to-do-list-app/src/components/feature/todo/todoList/todoItem.tsx
+++ to-do-list-app/src/components/feature/todo/todoList/todoItem.tsx
@@ -1,5 +1,5 @@
 import {TodoListItemResponse} from "../../../../service/todos/types.ts";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 
 function TodoItem({
