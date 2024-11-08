f872744ca6bb9c01a99b1e1fdd8d1140507367ed
jjalseu <cjfwns966@gmail.com>
2024-11-09 01:19:52 +0900

    - 로그인 체크를 전역 컴포넌트에서 하지 않고 router loader에서 해결

--- to-do-list-app/src/router/index.tsx
+++ to-do-list-app/src/router/index.tsx
@@ -1,19 +1,24 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, redirect} from "react-router-dom";
 import Login from "../page/login/login.tsx";
 import Signup from "../page/signup/signup.tsx";
 import Todo from "../page/todo/todo.tsx";
 import Root from "../page/root/root.tsx";
 import TodoDetails from "../page/todo/details/todoDetails.tsx";
 import TodoRegister from "../page/todo/register/todoRegister.tsx";
+import {authStorage} from "../helper/auth/authStorage.ts";
+import {authCheck} from "../helper/auth/authCheck.ts";
+import {RouteName} from "./routeName.ts";
 
 const router = createBrowserRouter([
     {
         path:"/",
         element:<Root/>,
+
         children:[
             {
                 path: "todo",
                 element:<Todo/>,
+                loader:async ({request}) => await authCheck({redirectUrlAfterPassAuth:request.url,redirectUrlAfterNotPassAuth:RouteName.LOGIN}),
                 children:[
                     {
                         path: ":id",
@@ -27,10 +32,12 @@ const router = createBrowserRouter([
             },
             {
                 path:"auth",
+                loader:async ({request}) => await authCheck({redirectUrlAfterPassAuth:request.url,redirectUrlAfterNotPassAuth:RouteName.LOGIN}),
                 children:[
                     {
                         path:"login",
-                        element:<Login/>
+                        element:<Login/>,
+
                     },
                     {
                         path:"signup",
