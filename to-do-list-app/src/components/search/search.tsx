5767014babe1b727c9a7f352c4b5f75cfd5aa2d0
jjalseu <cjfwns966@gmail.com>
2024-11-15 04:05:28 +0900

    - search.tsx > 의존성을 상위 컴포넌트로 전달, props를 통해 추상화

--- to-do-list-app/src/components/search/search.tsx
+++ to-do-list-app/src/components/search/search.tsx
@@ -1,26 +1,34 @@
-import {useState} from 'react';
-import {useSearchParams} from "react-router-dom";
 import CustomButton from "../button/customButton.tsx";
 import CustomInput from "../input/customInput.tsx";
 
+interface ISearch {
+    onSearch: (keyword: string) => void; // 검색
+    onReset: () => void; // 검색 초기화
+    keyword: string; // 검색어
+    onKeywordChange: (keyword: string) => void; // 검색어 변경
+}
+
 // 검색 컴포넌트
 // 역할 : 검색어를 입력받아 검색어를 쿼리스트링으로 설정하고, 검색어를 초기화하는 기능을 제공한다.
-function Search() {
+// 이 컴포넌트의 OCP 원칙에 잘 따르고 있을까?
+// 이 컴포넌트의 SRP 원칙에 잘 따르고 있을까?
+function Search({onSearch,onReset,keyword,onKeywordChange}:ISearch) {
 
-    const [_,setSearchParams] = useSearchParams()
+    // const [_,setSearchParams] = useSearchParams()
 
-   const [keyword,setKeyword] = useState<string>("")
     // 검색
     async function handleSearch(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
-        setSearchParams({
-            keyword
-        })    }
+        onSearch(keyword)
+        // setSearchParams({
+        //     keyword
+        // })
+    }
 
     // 검색 초기화
     async function handleReset() {
-            setSearchParams({}); // 쿼리스트링을 빈 객체로 설정하여 모든 쿼리스트링 제거
-            setKeyword(""); // 검색어 초기화
+            // setSearchParams({}); // 쿼리스트링을 빈 객체로 설정하여 모든 쿼리스트링 제거
+            // setKeyword(""); // 검색어 초기화
             }
 
     return (
@@ -36,20 +44,19 @@ function Search() {
                 <CustomInput
                     label={""}
                     value={keyword}
-                    onChange={value =>setKeyword(value)}
+                    onChange={onKeywordChange}
                     inputType={"text"}
                 />
                 {/*검색어를 쿼리스트링으로 설정*/}
                 <CustomButton
                     disabled={keyword === ""}
                     label={"검색"}
-                    onClick={() => setSearchParams({keyword})}
                     type={"submit"}
                 />
                 {/*초기화*/}
                 <CustomButton
                     label={"초기화"}
-                    onClick={handleReset}
+                    onClick={onReset}
                     type={"button"}
                 />
             </section>
