321a9234fd9e803b85f0da4f0a01ba0aaefb5e94
jjalseu <cjfwns966@gmail.com>
2024-11-14 12:48:42 +0900

    - 할일 목록 응답 DTO > 가공 필요없는 필드 readonly 필드로 변경

--- to-do-list-app/src/components/feature/todo/todoList/todoListTable.tsx
+++ to-do-list-app/src/components/feature/todo/todoList/todoListTable.tsx
@@ -1,7 +1,5 @@
 import {columns} from "../../../../constant/feature/todo/constant.ts";
-import {formatDate} from "../../../../helper/date.ts";
 import {useQueryTodos} from "../../../../service/todos/query.todos.ts";
-import {PriorityToKOEnum} from "../../../../type/feature/todo/types.ts";
 import CustomTable from "../../../table/customTable.tsx";
 import {useSortHandler} from "../../../table/sort/useSortHandler.ts";
 
@@ -16,14 +14,7 @@ function TodoListTable() {
     /**
      * @todo rows를 계산해주는 레이어를 API 호출단계에서 분리해주기
      */
-    const rows = todoList?todoList.map((todoItem) => ({
-        id: todoItem.id,
-        title: todoItem.title,
-        content: todoItem.content,
-        priority: PriorityToKOEnum[todoItem.priority],
-        createdAt: formatDate(new Date(todoItem.createdAt)),
-        updatedAt: formatDate(new Date(todoItem.createdAt))
-    })):[]
+    const rows = todoList?todoList:[]
 
 
 
--- to-do-list-app/src/service/todos/response/TodoListProcessResponse.ts
+++ to-do-list-app/src/service/todos/response/TodoListProcessResponse.ts
@@ -3,28 +3,21 @@ import {TodoListItemResponse} from "../types.ts";
 
 // API 응답받은 데이터를 클라이언트에서 원하는 방식으로 가공
 export class TodoListProcessResponse{
+    public readonly id: string;
+    public readonly title: string;
+    public readonly content: string;
     private todoItem:TodoListItemResponse
 
     constructor(todoItem:TodoListItemResponse){
         this.todoItem = todoItem
-    }
-    // 할일 아이디
-    get id(){
-        return this.todoItem.id
-    }
-
-    // 할일 제목
-    get title(){
-        return this.todoItem.title
+        this.id = todoItem.id; // 할일 아이디
+        this.title = todoItem.title; // 할일 제목
+        this.content = todoItem.content;// 할일 내용
     }
 
-    // 할일 내용
-    get content(){
-        return this.todoItem.content
-    }
 
     // 우선 순위 영어 => 한글로 변환
-    get priority(){
+     get priority(){
         return PriorityToKOEnum[this.todoItem.priority]
     }
 
