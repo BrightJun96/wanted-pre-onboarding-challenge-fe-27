fcdb717db2ca9a1109b770fcae31bfa55c275bbb
jjalseu <cjfwns966@gmail.com>
2024-11-14 17:05:54 +0900

    - TodoService 통합 인터페이스

--- to-do-list-app/src/service/todos/interface.todos.ts
+++ to-do-list-app/src/service/todos/interface.todos.ts
@@ -1,4 +1,6 @@
-// 할일 생성 서비스 인터페이스
+import {AddTodoRequest, EditTodoRequest, TodoListItemResponse, TodoListRequest} from "./types.ts";
+
+// 할일 등록 서비스 인터페이스
 export interface CreateTodoService<TAddRequest>{
     createTodo(request:TAddRequest) : Promise<Response>
 }
@@ -13,7 +15,12 @@ export interface DeleteTodoService{
     deleteTodo(id:string): Promise<Response>
 }
 
+// 할일 조회 서비스 인터페이스
 export interface ReadTodoService<TListRequest,TListResponse>{
     getTodos(request:TListRequest): Promise<TListResponse>
     getTodoById(id:string): Promise<TListResponse>
 }
+
+
+// 할일 CRUD 서비스 인터페이스
+export interface CRUDTodoService extends ReadTodoService<TodoListRequest, TodoListItemResponse>,CreateTodoService<AddTodoRequest>,UpdateTodoService<EditTodoRequest>,DeleteTodoService{}
