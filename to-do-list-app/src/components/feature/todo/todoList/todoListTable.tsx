4b3e1125a7af8b35d45ed3c15f636b55a42bee55
jjalseu <cjfwns966@gmail.com>
2024-11-18 18:53:57 +0900

    - 테이블 컴포넌트 셀 클릭시 이벤트 추가 - 할일 목록 테이블 컴포넌트에서 아이템 클릭하면 상세 컴포넌트 정보 우측에 나타내기

--- to-do-list-app/src/components/feature/todo/todoList/todoListTable.tsx
+++ to-do-list-app/src/components/feature/todo/todoList/todoListTable.tsx
@@ -1,7 +1,9 @@
+import {CellClickArgs} from "react-data-grid";
+import {useNavigate} from "react-router-dom";
 import {columns} from "../../../../constant/feature/todo/constant.ts";
 import {useQueryTodos} from "../../../../service/todos/query.todos.ts";
 import Fetcher from "../../../fetcher/fetcher.tsx";
-import CustomTable from "../../../table/customTable.tsx";
+import CustomTable, {TableRow} from "../../../table/customTable.tsx";
 import {useSortHandler} from "../../../table/sort/useSortHandler.ts";
 
 // 할일 목록 테이블
@@ -11,12 +13,20 @@ function TodoListTable() {
 
    const {data:todoList=[]} =  useQueryTodos()
 
+   const navigate =  useNavigate()
+
+    // 셀 클릭
+    function handleCellClick(params:CellClickArgs<TableRow>){
+        navigate(`/todo/${params.row.id}`)
+   }
+
     return (
         <Fetcher query={useQueryTodos} >
           <CustomTable
                 rows={todoList}
                 columns={columns}
                 handleSortChange={handleSortChange}
+                handleCellClick={handleCellClick}
             />
         </Fetcher>
     );
--- to-do-list-app/src/components/table/customTable.tsx
+++ to-do-list-app/src/components/table/customTable.tsx
@@ -1,6 +1,6 @@
 import 'react-data-grid/lib/styles.css';
 
-import DataGrid, {SortColumn} from 'react-data-grid';
+import DataGrid, {CellClickArgs, SortColumn} from 'react-data-grid';
 import useSortColumn from "./sort/useSortColumn.ts";
 
 export interface TableColumn{
@@ -9,25 +9,38 @@ export interface TableColumn{
     sortable?:boolean;
 }
 
+export interface TableRow{
+    [key: string]: any;
+}
+
 interface CustomTableProps {
     columns: TableColumn[];
-    rows: { [key: string]: any }[];
+    rows:TableRow[];
     handleSortChange?: (sortColumns: SortColumn[]) => void;
+    handleCellClick?: (params:CellClickArgs<TableRow>) => void;
 }
 
 function CustomTable({
     columns,
     rows,
-                         handleSortChange
+                         handleSortChange,
+    handleCellClick
                      }:CustomTableProps) {
 
 
- const {sortColumns,onSortColumnsChange}  = useSortColumn(handleSortChange)
+    const {sortColumns,onSortColumnsChange}  = useSortColumn(handleSortChange)
 
+    // 셀 클릭
+    function onCellClick(params:CellClickArgs<TableRow>){
+        if(handleCellClick){
+            handleCellClick(params)
+        }
+    }
     return (
         <DataGrid
             defaultColumnOptions={{ width: '1fr' }}
             sortColumns={sortColumns}
+            onCellClick={onCellClick}
             onSortColumnsChange={onSortColumnsChange}
         columns={columns}
         rows={rows}
